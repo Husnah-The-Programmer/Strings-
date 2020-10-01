a81190eae9b8f37cd3854065be59f3b5c42c41e1
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:19:20 +0100

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String args[]) {
+
+        // Create a string variable using the new keyword
+        String our = new String();
+
+        our = "Strings in Java";
+        System.out.println(our);
+    }
+}
